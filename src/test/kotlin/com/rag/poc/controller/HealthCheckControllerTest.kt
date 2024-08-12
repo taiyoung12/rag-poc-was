634c77e32e3ba00553fc4b2824c9e94696127fd8
taiyoung <qq3222@naver.com>
2024-08-12 22:19:58 +0900

    Remove: RestDocs 제거

--- src/test/kotlin/com/rag/poc/controller/HealthCheckControllerTest.kt
+++ src/test/kotlin/com/rag/poc/controller/HealthCheckControllerTest.kt
@@ -1,10 +1,9 @@
 package com.rag.poc.controller
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
-import org.springframework.restdocs.operation.preprocess.Preprocessors.*
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -17,15 +16,12 @@ class HealthCheckControllerTest {
 
     @Test
     fun `ping should return pong`() {
-        mockMvc.perform(get("/ping"))
-            .andExpect(status().isOk)
-            .andExpect(content().string("pong"))
-            .andDo(
-                document(
-                    "ping",
-                    preprocessRequest(prettyPrint()),
-                    preprocessResponse(prettyPrint()),
-                ),
-            )
+        val actual =
+            mockMvc.perform(get("/ping"))
+                .andExpect(status().isOk)
+                .andExpect(content().string("pong"))
+                .andReturn()
+
+        assertEquals("pong", actual.response.contentAsString)
     }
 }
