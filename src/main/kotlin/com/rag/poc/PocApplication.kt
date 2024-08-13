0915ea4c7e6773e4c61b330bf7257deadcf5b962
taiyoung <qq3222@naver.com>
2024-08-13 17:53:31 +0900

    Remove: FeignCleitn path 삭제

--- src/main/kotlin/com/rag/poc/PocApplication.kt
+++ src/main/kotlin/com/rag/poc/PocApplication.kt
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableFeignClients(basePackages = ["com.rag.poc.util"])
+@EnableFeignClients
 class PocApplication
 
 fun main(args: Array<String>) {
